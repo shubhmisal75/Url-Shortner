037d027b7d87530430625ed8d0348503c81c7928
shubham misal <shubhmisal75@gmail.com>
2022-03-01 12:21:16 +0530

    wishup code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-
 const express = require('express');
-var bodyParser = require('body-parser');
-
-const route = require('./routes/route.js');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const route = require('./routes/route');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://monty-python:SnYUEY4giV9rekw@functionup-backend-coho.0zpfv.mongodb.net/shubhmisal?authSource=admin&replicaSet=atlas-60843q-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", { useNewUrlParser: true })
+    .then(() => console.log('mongodb running and connected'))
+    .catch(err => console.log(err))
 
-mongoose.connect("mongodb+srv://monty-python:SnYUEY4giV9rekw@functionup-backend-coho.0zpfv.mongodb.net/shubhmisal?authSource=admin&replicaSet=atlas-60843q-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {useNewUrlParser: true})
-    .then(() => console.log('mongodb connected and Running...'))
-    .catch(err => console.log(err)) 
-
+    
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-	console.log('Express app running on port ' + (process.env.PORT || 3000))
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
